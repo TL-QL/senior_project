d97990116f83fa3f8ed3f726fe0542ca5b0bf193
TooLazy-QL <qiwenluo98@gmail.com>
2020-09-20 16:25:17 -0400

    Add Contact us UI

--- wowowui/src/components/MainComponent.js
+++ wowowui/src/components/MainComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Header from './HeaderComponent';
 import Home from './HomeComponent';
 import About from './AboutComponent';
+import Contact from './ContactComponent';
+import ProfileSeller from './ProfileSellerComponent';
 import Footer from './FooterComponent';
 import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
 
@@ -14,6 +16,8 @@ class Main extends Component {
                 <Switch>
                     <Route path="/home" component={Home} />
                     <Route exact path="/aboutus" component={About}/>
+                    <Route exact path="/contactus" component={Contact}/>
+                    <Route exact path="/profileseller" component={ProfileSeller}/>
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
