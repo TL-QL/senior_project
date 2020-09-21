12d9ab02cdf481cf286dbe63d70d703ce721a6a3
TooLazy-QL <qiwenluo98@gmail.com>
2020-09-21 02:04:08 -0400

    Add search bar to home page

--- wowowui/src/components/HomeComponent.js
+++ wowowui/src/components/HomeComponent.js
@@ -3,7 +3,11 @@ import React from 'react';
 function Home(props){
     return(
         <div className="container">
-            <h1>Home Page</h1>
+            <form className="input-container">
+                <i className="fa fa-search fa-lg icon"></i>
+                <input type="text" className="search-bar" name="search" placeholder="Search.." />
+                <button type="submit" class="searchButton"><b>Search</b></button>
+            </form>
         </div>
     );
 }
