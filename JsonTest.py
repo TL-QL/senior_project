be4f01c00cf08522ac845ffeedd18bc08d23fa14
Lina-Dong <59942753+Lina-Dong@users.noreply.github.com>
2020-12-10 17:03:23 -0500

    Update JsonTest.py

--- JsonTest.py
+++ JsonTest.py
@@ -1,84 +1,19 @@
-import numpy as np
 import json
 import sys
-# test run on MongoDB terminal with input commend:
-# python3 CreditScoringCalculator.py '{"seller": "ABC"}'
-# and the program outputs this:
-# {"seller": "ABC", "credit": 8.8}
 
-
-def infoIntegrityCompute(inputItem):
-    value = 5 # default score for infoIntegrity
-    if inputItem['sizeInfo']=='':
-        value -= 1
-    if inputItem['detachable']=='':
-        value -= 1
-    if inputItem['careIns']=='':
-        value -= 1
-    if inputItem['productInsurance']=='':
-        value -= 1
-    if inputItem['damage']=='':
-        value -= 1
-    return value
-
-
-def readJSON():
-    # the input json object is info about 'one' sold item
-    # items = localTest()
-    items = json.loads(sys.argv[1])
-    # inputList = [informationIntegrity,purchaseExperience,serviceExperience,pictureQuality]
-    inputList = []
-    if items['comments']!=[]: # check if there is feedback value
-        # print("is sold out and comments non zero")
-        inputList.append(infoIntegrityCompute(items))
-        purchase = 0.0
-        service = 0.0
-        for i in range(len(items['comments'])):
-            purchase = purchase + items['comments'][i]['rating']
-            server = server + items['comments'][i]['serviceRating']
-        purchase = purchase / len(items['comments'])
-        service = service / len(items['comments'])
-        inputList.append(purchase)
-        inputList.append(service)
-        inputList.append(items['imageScore'])
-    return inputList, items['seller']
-
-
-def outputJsonObject(seller, credit):
-    # output to the terminal
-    # seller is str, credit is float
-    # since the site use 10 credit scale, double and then round to int
-    thisJson = {
-        "seller": seller,
-        "credit": int(round(credit*2))
-    }
-    return thisJson
-    
-
-
-thiscredit, thisseller= readJSON()
-
-thisJson = {
-        "seller": thisseller,
-        "credit": int(round(3.5*2))
-    }
-jsonOut = json.dumps(thisJson)
-sys.stdout.write(jsonOut)
+# fileObject2 = open('D:/aCSDS395/pythonReadWrite/itemsSample.json')
+# filename = sys.argv[1] # python JsonTest.py D:/aCSDS395/pythonReadWrite/itemsSample.json
+# fileObject2 = open(filename)
+# testinput = json.load(fileObject2)
+testinput = json.loads(sys.argv[1]) # this should be a json object
+testx = {
+    "seller": testinput['seller'],
+    "credit": 8.8
+}
+testoutput = json.dumps(testx)
+print(testoutput)
+sys.stdout.write(testoutput)
 sys.exit(0)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# sample running phrase in terminal:
+# python JsonTest.py (the input JsonObject) > tempout.txt
+# so the content will stored in tempout.txt, you may use other format of file
