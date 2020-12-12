e059c618be0e9047a979814bf15685397f092587
Lina-Dong <59942753+Lina-Dong@users.noreply.github.com>
2020-12-11 20:30:20 -0500

    update latest version of NN_out.txt, and updated JsonTest

--- JsonTest.py
+++ JsonTest.py
@@ -6,11 +6,10 @@ import sys
 # and the program outputs this:
 # {"seller": "ABC", "credit": 8.8}
 
-#
 
 def readNN():
     # read the .txt files(that store NN output) from local
-    fileObject1 = open('D:/aCSDS395/pythonReadWrite/NN_output.txt')
+    fileObject1 = open(r"NN_output.txt")
     outputMatrix = fileObject1.read()
     arrayOfMatrix = outputMatrix.split('[')
     matrixList = []
@@ -48,13 +47,6 @@ def infoIntegrityCompute(inputItem):
     return value
 
 
-def localTest():
-    fileObject = open('D:/aCSDS395/pythonReadWrite/itemsSample.json')
-    # load the first item. in real use, would be the one received from terminal
-    items = json.load(fileObject)[1]
-    return items
-
-
 def terminalTest():
     # return the json object as input argv from terminal
     return json.loads(sys.argv[1])
@@ -72,28 +64,14 @@ def readJSON():
         purchase = 0.0
         service = 0.0
         for i in range(len(items['comments'])):
-            purchase = purchase + items['comments'][i]['rating']
-            service = service + items['comments'][i]['serviceRating']
+            purchase = purchase + (items['comments'][i]['rating'] / 2)
+            service = service + (items['comments'][i]['serviceRating'] /2)
         purchase = purchase / len(items['comments'])
         service = service / len(items['comments'])
         inputList.append(purchase)
         inputList.append(service)
         inputList.append(items['imageScore'])
-        # soldoutList.append(inputList)
-        # correspondingSeller.append(items['seller'])
-    # return soldoutList, correspondingSeller
-    return inputList, items['seller']
-
-
-def writeJSON(correspondingSeller, correspondingCredit):
-    with open("D:/aCSDS395/pythonReadWrite/usersSample.json", "r") as fileObject:
-        users = json.load(fileObject)
-    for i in range(len(users)):
-        for j in range(len(correspondingSeller)):
-            if users[i]['username']==correspondingSeller[j]:
-                users[i]["credit"] = correspondingCredit[j]
-    with open("D:/aCSDS395/pythonReadWrite/usersSample.json", "w") as fileObject:
-        json.dump(users, fileObject)
+    return inputList, items['seller'], items['credit']
 
 
 def outputJsonObject(seller, credit):
@@ -111,6 +89,22 @@ def outputJsonObject(seller, credit):
     sys.stdout.flush()
 
 
+def provideInt(creditarray, finalcredit):
+    num = 0
+    for i in creditarray:
+        num += i
+    num = num / len(creditarray)
+    num = num * 2
+    print(num)
+    if num >= finalcredit:
+        num = (num + finalcredit)/2
+    else:
+        num = (num + finalcredit)/2
+        num = num // 1
+    num = int(np.round(num))
+    return num
+
+
 def sigmoid(x):
     x=np.transpose(x)
     if np.size(x) == 1:
@@ -121,6 +115,7 @@ def sigmoid(x):
             k[i] = (1/(1 + np.exp(-x[i])))
         k=(k)
     return k
+
 
 def computeCredits(creditarray, weightarray): # input: credit array, weight array, bias array; output: float
     creditarray=np.concatenate(([1], creditarray), axis=0)
@@ -133,35 +128,28 @@ def computeCredits(creditarray, weightarray): # input: credit array, weight arra
 
     # readNN()[0][0] 3X1 vetor u_matrice
     u_matrice[0] = np.dot((weightarray[0]),creditarray.T) # weight[1] * input_pattern
-    # print("start:",u_matrice[0])
-    # print(weightarray[0])
-    # print(weightarray[1])
-    # print(weightarray[2])
 
     phi_matrice[0] = sigmoid(u_matrice[0])
-    # print("this is :", phi_matrice[0])
-    # print(type(phi_matrice[0]))
     phi_matrice[0][0]= 1
     # u_matrice[0] [0]= 1
     for i in range(1,len(weightarray)):
         u_matrice[i] =  np.dot((weightarray[i]),phi_matrice[i-1])
         phi_matrice[i] = sigmoid(u_matrice[i])
     credit = phi_matrice[len(phi_matrice)-1]
-    # print("this",credit*5)
-    return credit*5
+    return credit*10
 
 
-def openSampleOut():
-    file = open('C:/itemsSample.json')
-    readout = file.read()
-    return readout
+def updatecredit(creditarray, finalcredit, currentcredit):
+    computevalue = provideInt(creditarray, finalcredit)
+    if currentcredit == -1:
+        return computevalue
+    else:
+        return (computevalue + currentcredit) / 2
 
-# weight = readNN()
-# thiscredit, thisseller = readJSON()
-# finalcredit = computeCredits(thiscredit,weight)
-# outputJsonObject(thisseller,finalcredit[0])
 
-thiscredit, thisseller = readJSON()
-sampleSeller = openSampleOut()
-outputJsonObject(sampleSeller, 3.5)
+weight = readNN()
+thiscredit, thisseller, currentcredit = readJSON()
+finalcredit = computeCredits(thiscredit,weight)
+finalvalue = updatecredit(thiscredit, finalcredit[0], currentcredit)
+outputJsonObject(thisseller,finalvalue)
 
